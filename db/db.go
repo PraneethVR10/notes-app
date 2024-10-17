59e6b2ed8103dee826866c35d61b369fd4052314
praneethvr10 <praneethvr03@gmail.com>
2024-10-17 08:54:56 +0530

    added db

---
+++ db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "notesdb"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Successfully connected to database!")
+	return db, nil
+}
